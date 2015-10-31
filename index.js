2001bd81ef892f97cfd718aa0cd2e6fec396320f
Joseph Balancio <jlbalancio@gmail.com>
2015-10-31 10:57:39 -0700

    Create initial library

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const Authorizer = require('./lib/Authorizer')
+
+module.exports = (options) => {
+	return new Authorizer(options)
+}
---
+++ test/index.js
@@ -0,0 +1,22 @@
+/* eslint-env mocha */
+'use strict'
+
+const chai = require('chai')
+const expect = chai.expect
+
+const authz = require('../')
+const Authorizer = require('../lib/Authorizer')
+
+describe('authz', () => {
+	describe('create', () => {
+		it('creates', () => {
+			const authorizer = authz()
+			expect(authorizer).to.be.an.instanceof(Authorizer)
+		})
+		it('creates with options', () => {
+			const authorizer = authz({foo: 'bar'})
+			expect(authorizer).to.have.property('options')
+				.that.has.property('foo', 'bar')
+		})
+	})
+})
